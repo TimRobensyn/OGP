53320d90d74fda63416275ad889c31eff8270f43
TimRobensyn <tim.robensyn@student.kuleuven.be>
2020-02-23 17:21:38 +0100

    Initial commit

---
+++ practicum1/file/File.java
@@ -0,0 +1,16 @@
+/**
+ * ToDo class commentaar
+ * 
+ * @version 1.0
+ * @author Tim Lauwers, Robbe Vanbiervliet, Tim Robensyn
+ */
+
+package file;
+
+public class File {
+	
+	public static void main(String[] Args) {
+		System.out.println("Eeeeeerste functie huyyyyy");
+	}
+
+}
