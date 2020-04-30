4cc9b83d3e63e67eb53b015532a364a91b5662e7
TimRobensyn <tim.robensyn@student.kuleuven.be>
2020-04-30 12:48:17 +0200

    Start examenproject Projectstructuur aangemaakt Eerste (voorlopig lege) klasse 'AlchemicIngredient' aangemaakt

---
+++ examenProject/src/alchemy/AlchemicIngredient.java
@@ -0,0 +1,14 @@
+package alchemy;
+
+import be.kuleuven.cs.som.annotate.*;
+
+/**
+ * A class of alchemic ingredients involving a name, temperature, type, quantity and a liquid or powder state.
+ * 
+ * @version  1.0
+ * @author   Robbe Van Biervliet, Tim Lauwers, Tim Robensyn
+ */
+
+public class AlchemicIngredient {
+
+}
