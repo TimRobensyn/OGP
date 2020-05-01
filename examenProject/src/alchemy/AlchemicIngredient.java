08383648b37621fed102791ee92cf904c6532761
TimLauwers <tim.lauwers@student.kuleuven.be>
2020-05-01 15:06:29 +0200

    Namen alfabetisch zetten (test push)
    want stiekem benk wa autistisch lol

--- examenProject/src/alchemy/AlchemicIngredient.java
+++ examenProject/src/alchemy/AlchemicIngredient.java
@@ -6,7 +6,7 @@ import be.kuleuven.cs.som.annotate.*;
  * A class of alchemic ingredients involving a name, temperature, type, quantity and a liquid or powder state.
  * 
  * @version  1.0
- * @author   Robbe Van Biervliet, Tim Lauwers, Tim Robensyn
+ * @author   Tim Lauwers, Tim Robensyn, Robbe Van Biervliet
  */
 
 public class AlchemicIngredient {
