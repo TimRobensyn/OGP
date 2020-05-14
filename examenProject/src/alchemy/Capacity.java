9e74fc7a0b4a9a5c9e10eb46f49345aac72f4e2e
Robbe Van Biervliet <Robbe Van Biervliet@MSI>
2020-05-14 18:40:40 +0200

    Ingredientcontainer en capacity changed

---
+++ examenProject/src/alchemy/Capacity.java
@@ -0,0 +1,39 @@
+package alchemy;
+
+import be.kuleuven.cs.som.annotate.*;
+
+/**
+ * A value class for quantities of ingredients.
+ * 
+ * @version	1.0
+ * @author	Tim Lauwers, Tim Robensyn, Robbe Van Biervliet
+ *
+ */
+
+public enum Capacity {
+	
+	SPOON(1), VIAL(5), BOTTLE(15), JUG(105), BARREL(1260), SACHET(7), BOX(42), SACK(126), CHEST(1260);
+	
+	/**\
+	 * Initialize this capacity with the given amount
+	 * @param amount
+	 * 		  The amount of this capacity
+	 */
+	@Raw
+	private Capacity(int amount) {
+		this.capacity = amount;
+	}
+	
+	/**
+	 * Returns the amount of this capacity
+	 */
+	@Basic @Raw @Immutable
+	public int getCapacity() {
+		return this.capacity;
+	}
+	
+	/**
+	 * Variable storing the amount of capacity
+	 */
+	private final int capacity;
+}
--- examenProject/src/alchemy/IngredientContainer.java
+++ examenProject/src/alchemy/IngredientContainer.java
@@ -3,7 +3,7 @@ package alchemy;
 import be.kuleuven.cs.som.annotate.*;
 
 /**
- * A class of ingredient containers involving a capacity type and contents in spoons.
+ * An enumeration class of ingredient containers involving a capacity type and contents in spoons.
  * 
  * @version	1.0
  * @author	Tim Lauwers, Tim Robensyn, Robbe Van Biervliet
@@ -12,11 +12,74 @@ import be.kuleuven.cs.som.annotate.*;
 
 public class IngredientContainer {
 	
-	public IngredientContainer(Quantity containerType, int contents) {
-		this.contents = contents;
-		this.containerType = containerType;
+	/**
+	 * Initialize this container with the given capacity and contents
+	 * @param capacity
+	 * 		  The capacity of this container
+	 * @param contents
+	 * 		  The contents in this container
+	 */
+	@Raw
+	public IngredientContainer(Capacity capacity, int contents) {
+		setCapacity(capacity);
+		setContents(contents);
 	}
 	
+	/************************************************************************
+	 * Capacity
+	 ************************************************************************/
+	
+	/**
+	 * Return the capacity of this container
+	 */
+	@Raw @Basic @Immutable
+	public int getCapacity() {
+		return this.capacity.getCapacity();
+	}
+	
+	/**
+	 * Set the capacity of this container to the given capacity
+	 * @param capacity
+	 * 		  The capacity of this container
+	 */
+	private void setCapacity(Capacity capacity) {
+		this.capacity = capacity;
+	}
+	
+	/**
+	 * Variable storing the capacity of this container
+	 */
+	private Capacity capacity;
+	
+	
+	/************************************************************************
+	 * Contents
+	 ************************************************************************/
+
+	/**
+	 * Returns the contents in this container
+	 */
+	@Raw @Basic
+	public int getContents() {
+		return this.contents;
+	}
+	
+	/**
+	 * Set the contents off this container to the given contents
+	 * @param contents
+	 * 		  The given contents
+	 * @pre The given contents is not less than 0 or greater than the capacity of this container
+	 *		| (contents >= 0) && (contents <= this.getCapacity())
+	 */
+	public void setContents(int contents) {
+		assert((contents >= 0) && (contents <= this.getCapacity())) :
+			"Precondition: Contents is not less than 0 or more than the container's capacity";
+		this.contents = contents;
+	}
+	
+	/**
+	 * Variable storing the contents in this container
+	 */
 	private int contents = 0;
-	private Quantity containerType;
+
 }
