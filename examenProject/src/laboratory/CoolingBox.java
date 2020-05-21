545b8ec8e95fbdc66bf3d45dc180168810c372f4
TimRobensyn <tim.robensyn@student.kuleuven.be>
2020-05-21 13:03:13 +0200

    Commentaar coolingBox

--- examenProject/src/laboratory/CoolingBox.java
+++ examenProject/src/laboratory/CoolingBox.java
@@ -13,9 +13,14 @@ import alchemy.*;
 public class CoolingBox extends TemperatureDevice {
 	
 	/**
-	 * Initialize a cooling box with the given temperature and startIngredient. //TODO
-	 * @param temperature
-	 * @param ingredient
+	 * Initialize a cooling box with the given temperature and startIngredient.
+	 * 
+	 * @param	temperature
+	 * 			The new temperature object to give to this new cooling box.
+	 * @param	ingredient
+	 * 			The new ingredient container to load in this new cooling box.
+	 * @effect	The new cooling box is a temperature device with a the given temperature and startIngredient.
+	 * 			| super(temperature,startIngredient)
 	 */
 	@Raw
 	public CoolingBox(Temperature temperature, IngredientContainer startIngredient) {
@@ -23,8 +28,12 @@ public class CoolingBox extends TemperatureDevice {
 	}
 	
 	/**
-	 * Andere constructor //TODO
-	 * @param temperature
+	 * Initialize a cooling box with the given temperature and startIngredient.
+	 * 
+	 * @param	temperature
+	 * 			The new temperature object to give to this new cooling box.
+	 * @effect	The new cooling box is a temperature device with a the given temperature and startIngredient.
+	 * 			| super(temperature)
 	 */
 	@Raw
 	public CoolingBox(Temperature temperature) {
@@ -32,7 +41,18 @@ public class CoolingBox extends TemperatureDevice {
 	}
 
 	/**
-	 * If the ingredient is hotter than //TODO
+	 * Cool the loaded ingredient in this cooling device and put it in the processedIngredient variable.
+	 * 
+	 * @post	If the temperature of the ingredient is higher than the temperature of the cooling box,
+	 * 			change the temperature to the temperature of the cooling box.
+	 * 			Put the loaded ingredient in the processed ingredient slot.
+	 * 			| if (Temperature.compareTemperature(getStartIngredient().getTemperatureObject(),
+	 * 			|	getTemperatureObject())==-1)
+	 * 			|     then (new.getProcessedIngredient().getTemperatureObject() == getTemperatureObject()
+	 * 			|			&& new.getStartIngredient() == null)
+	 * 			| else
+	 * 			|   (new.getProcessedIngredient() == getStartIngredient()
+	 * 			|	 && new.getStartIngredient() == null)
 	 */
 	@Override
 	public void process() {
